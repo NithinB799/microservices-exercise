debda6fc95c1add43e923e3c29d3e10ef02e55a8
NithinB799 <baddamnithinreddy799@gmail.com>
2026-06-24 18:40:21 -0400

    1J-Kafka-Consumer

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -1,13 +1,14 @@
 package com.example.productservice;
 
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableKafka
 @SpringBootApplication
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
 }
