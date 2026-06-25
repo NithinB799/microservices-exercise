3ff53dd62b90614f4784b96b64bd6eb0082060ed
NithinB799 <baddamnithinreddy799@gmail.com>
2026-06-25 16:40:29 -0400

    2A-react18-setup

---
+++ frontend-service/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <div>
+        <h1>Enterprise Java Microservices Frontend</h1>
+        <p>React frontend setup is working successfully.</p>
+      </div>
+  )
+}
+
+export default App
