ea60f2dd311bdf2b6fb05a3c4c268e927216fae6
NithinB799 <baddamnithinreddy799@gmail.com>
2026-06-25 20:14:33 -0400

    2H-routing

--- frontend-service/src/App.jsx
+++ frontend-service/src/App.jsx
@@ -1,21 +1,22 @@
-import ProductListPage from './pages/ProductListPage';
-import AddProductPage from './pages/AddProductPage';
-import CartPage from './pages/CartPage';
+import { Link } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
     return (
         <div>
             <h1>Enterprise Product Management</h1>
 
-            <AddProductPage />
+            <nav>
+                <Link to="/products">Products</Link>
+                {' | '}
+                <Link to="/add-product">Add Product</Link>
+                {' | '}
+                <Link to="/cart">Cart</Link>
+            </nav>
 
             <hr />
 
-            <ProductListPage />
-
-            <hr />
-
-            <CartPage />
+            <AppRoutes />
         </div>
     );
 }
---
+++ frontend-service/src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ProductListPage from '../pages/ProductListPage';
+import AddProductPage from '../pages/AddProductPage';
+import CartPage from '../pages/CartPage';
+
+function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/products" />} />
+            <Route path="/products" element={<ProductListPage />} />
+            <Route path="/add-product" element={<AddProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
+        </Routes>
+    );
+}
+
+export default AppRoutes;
